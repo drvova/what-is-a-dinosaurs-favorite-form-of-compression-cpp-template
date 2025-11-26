9b7d368acb87d6c0341ba9511c155af6a9ae0167
drvova <drvova@openwarp.local>
2025-11-25 23:25:15 -0500

    up

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
-
 #include "src/factorial.hpp"
 
 int main() {
-  std::cout << "The factorial of 5 is " << factorial(5) << std::endl;
+    std::string s = "abc";
+    auto r = p(s);
+    auto d = q(r.d, r.m);
+    
+    std::cout << "O: " << s << std::endl;
+    std::cout << "C: " << r.d << std::endl;
+    std::cout << "D: " << d << std::endl;
+    std::cout << "M: " << (s == d ? "Y" : "N") << std::endl;
+    
+    return 0;
 }
